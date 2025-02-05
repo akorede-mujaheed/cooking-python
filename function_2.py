260a34f5561f1f734c1fa9a732c31c2667e029b4
akorede-mujaheed <akoredemujaheed979@gmail.com>
2025-02-05 13:59:01 +0100

    clean _up

--- function_2.py
+++ function_2.py
@@ -1,9 +1,11 @@
 def greating(name):
     print("Hello" + name )
 
+
 greating("alice!")
 
 def sum_of_two_number(number1, number2):
     print("the sum of the number is ", number1 + number2 )
+
 
 sum_of_two_number(3,4) 
