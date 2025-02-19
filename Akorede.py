69a03cf5bdd71c54b3b412b1897c35f4e1f20113
akorede-mujaheed <akoredemujaheed979@gmail.com>
2025-02-19 10:40:39 +0100

    done

--- Akorede.py
+++ Akorede.py
@@ -1,4 +1,21 @@
 import math 
-r=5
-v=(4/3)*(math.pi)*(r**3)
-print(v)
+from math import cos, sin, e
+
+r = 5
+volume =(4/3)*(math.pi)*(r**3)
+
+
+x = 42
+cosx = cos(x)
+sinx = sin(x)
+cosx_square = cosx**2
+sinx_square = sinx**2
+
+
+y = 2
+exponacial_squeared = math.e**2
+power_exponacial = math.pow(e, 2)
+math_exponecial = math.exp(y)
+ 
+print(volume, cosx_square+sinx_square, exponacial_squeared, power_exponacial, math_exponecial )
+
