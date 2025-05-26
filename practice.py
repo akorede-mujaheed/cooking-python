ff164ab90ab50594e739221c44c2304eaba5cc07
akorede-mujaheed <akoredemujaheed979@gmail.com>
2025-05-26 11:36:41 +0100

    comparism_done

--- practice.py
+++ practice.py
@@ -1,13 +1,21 @@
+#  to_check_if_the_number_is_old_or_even 
+
+# input_value 
+
 value = 9
+
+# give_result_of_the_value
 
 result = value % 2!= 0
 
 print (result)
 
-if(result):
-    print( value, "is an old number ")
+# specify_if_old_or_even
 
+if(result):
+    
+    print( value, "is an old number ")
 else:
-     print(value, "is an even number")
+    print(value, "is an even number")
 
      
