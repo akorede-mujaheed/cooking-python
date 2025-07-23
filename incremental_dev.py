43db18e88da9ec86b1bbdccf944c7ca7ce531de4
akorede-mujaheed <akoredemujaheed979@gmail.com>
2025-07-23 10:32:38 +0100

    solved

---
+++ incremental_dev.py
@@ -0,0 +1,25 @@
+def distance(x1, y1, x2, y2):
+    return 0.0
+distance(1, 2, 4, 6)
+print(distance(1, 2, 4, 6))
+
+
+def distance(x1, y1, x2, y2):
+    dx = x2 -x1
+    dy = y2 -y1
+    print("dx is ", dx)
+    print("dy is ", dy)
+    return 0.0
+
+print(distance(1, 2, 4, 6))
+
+
+def distance(x1, y1, x2, y2):
+    dx = x2-x1
+    dy = y2-y1
+    disquared = dx**2 + dy**2
+    print("disquared is: ", disquared)
+    return 0.0
+print(distance(1, 2, 4, 6))
+
+
